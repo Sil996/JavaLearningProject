fdaf7c0b18550d8268bd80819d200d10f0fdb049
Silviu <triscasilviu@gmail.com>
2024-10-17 21:54:47 +0300

    Adde the Variables and Comments Task.

---
+++ src/main/java/md/otp/variableandcomments/VariablesAndComments.java
@@ -0,0 +1,48 @@
+package md.otp.variableandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        // Declare the primitive data type variables
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntLongDataType;
+        int upperLimitOfIntLongDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntLongDataType = -2147483648;
+        upperLimitOfIntLongDataType = 214748364;
+        lowerLimitOfLongDataType = -922337203;
+        upperLimitOfLongDataType = 92337203;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308;
+        upperLimitOfDoubleDataType = 1.7E+308;
+
+        System.out.println(upperLimitOfFloatDataType);
+
+        // CTRL + ALT + L - pretty code
+    }
+
+}
